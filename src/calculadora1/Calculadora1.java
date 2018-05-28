f680b7a23784ab259e5996b2f5607645f2c41577
Edwin <Edwin@Edwin-PC>
2018-05-27 19:00:46 -0500

    Inicio proyecto

---
+++ src/calculadora1/Calculadora1.java
@@ -0,0 +1,19 @@
+package calculadora1;
+
+/**
+ *
+ * @author Edwin
+ */
+public class Calculadora1 {
+
+    private int ans;
+
+    public Calculadora1() {
+        ans = 0;
+    }
+
+    public int suma(int a, int b) {
+        ans = a + b;
+        return ans;
+    }
+}
---
+++ test/calculadora1/Calculadora1Test.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora1;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Edwin
+ */
+public class Calculadora1Test {
+    
+    public Calculadora1Test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of suma method, of class Calculadora1.
+     */
+    @Test
+    public void testSuma() {
+        System.out.println("suma");
+        int a = 3;
+        int b = 2;
+        Calculadora1 instance = new Calculadora1();
+        int expResult = 5;
+        int result = instance.suma(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
